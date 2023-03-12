3610a8e0bbf934f267ca6517902c875b10c47399
Ricardo Zamudio <46466785+JustZeus@users.noreply.github.com>
2023-03-12 17:07:25 -0600

    added relay control functions

--- AVR_Controller/commander.c
+++ AVR_Controller/commander.c
@@ -33,6 +33,7 @@ void command_switcher(char *command){
 	if(strcmp(main_command, "ctrl") == 0)
 		{
 			printf("OK, CONTROL\r\n");
+			set_relay_states(command);
 			clear_command_buffer(command);
 		}
 	else if (strcmp(main_command, "show") == 0)
@@ -44,11 +45,12 @@ void command_switcher(char *command){
 		{
 			printf("\r\nAVR-CONTROLLER, Version: 0.0.1, Ricardo Zamudio C. \r\n Command list: \r\n [ctrl] -controls the relay's states \r\n [show] -displays the actual states \r\n");
 			clear_command_buffer(command);
-		}else{
+		} 
+	else 
+		{
 			send_help_info();
 			clear_command_buffer(command);
 		}
-
 }
 
 
@@ -57,4 +59,38 @@ void clear_command_buffer(char *command){
 	{
 		command[i]=0x00;
 	}
+}
+
+
+void set_relay_states(char *command){
+
+	const char delimeter = ',';
+
+	char cursor = MAIN_COMMAND_LENGTH + 1;
+	char [2] number;
+	int index = 0;
+	int is_a_two_digit_number = 0;
+	*char[] end_char;
+
+	while(end_char == BLANK_SPACE || NEW_LINE || RETURN_CARRIAGE)
+	{
+		number[index] = command[cursor];
+		cursor++;
+		index++;
+
+		if(index==1){
+			is_a_two_digit_number=1;
+		}	
+
+		if(command[cursor] == delimeter || BLANK_SPACE)
+		{		
+			index=0;
+			printf(number);
+			printf("\r\n");
+		}
+		
+		end_char=command[cursor];
+	
+	}
+	
 }
--- AVR_Controller/commander.h
+++ AVR_Controller/commander.h
@@ -13,11 +13,20 @@
 
 #define MSG_OK "\r\nOK\r\n"
 #define MAIN_COMMAND_LENGTH 5
+#define NUMBER_OF_RELAYS 4
+
+#define BLANK_SPACE " "
+#define NEW_LINE "\n"
+#define RETURN_CARRIAGE "\r"
 
 void executeCommand(char *command);
 void send_help_info();
 void command_switcher(char *command);
 void clear_command_buffer(char *command);
+
+// Control functions:
+void set_relay_states(char *command);
+void relay_state_changer(char relay_id, char state, int is_a_two_digit_number);
 
 
 
